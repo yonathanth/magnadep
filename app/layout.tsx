ab4d3c097f63569d624e81525be070c1f610cf2c
yonathanth <yonathantenaye@gmail.com>
2025-05-03 16:14:50 +0300

    changed favicon.ico to icon.ico

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,8 +35,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Add your favicon here */}
-        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body
